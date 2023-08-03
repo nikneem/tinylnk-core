641aa1f052713630c4fedeb8e7869fd3ed8b8e2d
Eduard Keilholz <e.keilholz@4dotnet.nl>
2023-08-03 16:03:55 +0200

    Add configuration and configuration validation from core

---
+++ src/TinyLink.Core/Configuration/AzureCloudConfigurationValidator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace TinyLink.Core.Configuration;
+
+public class AzureCloudConfigurationValidator : IValidateOptions<AzureCloudConfiguration>
+{
+        public ValidateOptionsResult Validate(string? name, AzureCloudConfiguration options)
+        {
+            var errorList = new List<string>();
+            if (string.IsNullOrWhiteSpace(options.StorageAccountName))
+            {
+                errorList.Add($"The app setting {AzureCloudConfiguration.SectionName}.StorageAccountName cannot be null or empty");
+            }
+            return errorList.Count > 0 ? ValidateOptionsResult.Fail(errorList) : ValidateOptionsResult.Success;
+        }
+}
---
+++ src/TinyLink.Core/ExtensionMethods/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TinyLink.Core.Configuration;
+
+namespace TinyLink.Core.ExtensionMethods;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddTinyLinksCore(this IServiceCollection services, IConfiguration configuration)
+    {
+        var configurationSection = configuration.GetSection(AzureCloudConfiguration.SectionName);
+
+        services.AddOptions<AzureCloudConfiguration>()
+            .Bind(configuration.GetSection(AzureCloudConfiguration.SectionName))
+            .ValidateOnStart();
+
+        services.AddApplicationInsightsTelemetry();
+
+        return services;
+    }
+}
