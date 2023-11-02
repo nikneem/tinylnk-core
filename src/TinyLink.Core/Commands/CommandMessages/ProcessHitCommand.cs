5fde5637abf60aa7d7320b78b093602a701a55db
Eduard Keilholz <e.keilholz@4dotnet.nl>
2023-11-02 15:28:39 +0100

    Adding the shortlink ID

--- src/TinyLink.Core/Commands/CommandMessages/ProcessHitCommand.cs
+++ src/TinyLink.Core/Commands/CommandMessages/ProcessHitCommand.cs
@@ -4,6 +4,7 @@ using TinyLink.Core.Abstractions.Commands;
 namespace TinyLink.Core.Commands.CommandMessages;
 
 public record ProcessHitCommand(
+    Guid Id,
     string ShortCode,
     string OwnerId,
     DateTimeOffset CreatedOn) : IUrlShortnerCommand
